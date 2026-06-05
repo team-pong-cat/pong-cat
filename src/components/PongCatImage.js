6fc81b1a3e4921bd8b34988caa1f841d6d148a5c
jdw09 <razion0304@naver.com>
2026-06-05 09:59:46 +0900

    feat: 키보드를 통한 클릭 구현

--- src/components/PongCatImage.js
+++ src/components/PongCatImage.js
@@ -1,18 +1,25 @@
+import { forwardRef } from 'react';
 import catClosed from '../assets/cat-closed.png';
 import catOpen from '../assets/cat-open.png';
 
-function PongCatImage({ isOpen, onMouseDown, onMouseUp }) {
+const PongCatImage = forwardRef(function PongCatImage(
+  { isOpen, onMouseDown, onMouseUp },
+  ref
+) {
   return (
     <img
-      className={`pong-cat-img${isOpen ? ' open' : ''}`}
+      ref={ref}
+      className={`outline-none pong-cat-img${isOpen ? ' open' : ''}`}
       src={isOpen ? catOpen : catClosed}
       alt="pong cat"
+      role="button"
+      tabIndex={0}
       onMouseDown={onMouseDown}
       onMouseUp={onMouseUp}
       onMouseLeave={onMouseUp}
       draggable={false}
     />
   );
-}
+});
 
 export default PongCatImage;
